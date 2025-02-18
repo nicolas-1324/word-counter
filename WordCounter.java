7e3a8086421c2e77c18cd3ba262c9a89f67bffb2
nicolas-1324 <nicolaspucer@gmail.com>
2025-02-18 19:16:55 +0100

    koncan skoraj

--- WordCounter.java
+++ WordCounter.java
@@ -1,9 +1,21 @@
-
+import java.util.*;
 
 public class WordCounter
 {
 	public static void main(String[] args)
 	{
-		
+		Scanner in = new Scanner(System.in);
+		int st_besed = 1;
+
+		System.out.print("Vnesi poljuben niz ki ima vec besed: ");
+		String niz = in.nextLine();
+
+		for(int i = 0; i<niz.length(); i++){
+			if(Character.isWhitespace(niz.charAt(i))){
+				st_besed++;
+			}
+		}
+
+		System.out.println(st_besed);
 	}
 }
