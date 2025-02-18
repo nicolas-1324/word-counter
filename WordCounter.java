3ab0b1376b6c7d7bf449aa1665ca146df9e72845
nicolas-1324 <nicolaspucer@gmail.com>
2025-02-18 23:31:17 +0100

    dodal konstruktorje za File in FileReader in logiko ki preveri ali je vnesen normalen niz ali pot do mape

--- WordCounter.java
+++ WordCounter.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileReader;
 
 public class WordCounter
 {
@@ -7,15 +9,28 @@ public class WordCounter
 		Scanner in = new Scanner(System.in);
 		int st_besed = 1;
 
-		System.out.print("Vnesi poljuben niz ki ima vec besed: ");
+		System.out.print("Vnesi poljuben niz ki ima vec besed, ali pot do mape: ");
 		String niz = in.nextLine();
-
+		
+		for(int i = 0; i<niz.length(); i++){
+			if(niz.charAt(i) == '/'){
+				try{
+					File potDoMape = new File(niz);
+					FileReader bralec = new FileReader(potDoMape);
+					System.out.println("succes!");
+				}
+				catch(Exception e){
+					System.out.println(e.getMessage());
+				}
+				break;
+			}
+		}
 		for(int i = 0; i<niz.length(); i++){
 			if(Character.isWhitespace(niz.charAt(i))){
 				st_besed++;
 			}
 		}
 
-		System.out.println(st_besed);
+		System.out.println("Stevilo besed v nizu: " + st_besed);
 	}
 }
