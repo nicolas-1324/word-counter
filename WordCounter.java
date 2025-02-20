e3e8f0e901ab9af9e7daf1271b1467e324506ca4
nicolas-1324 <nicolaspucer@gmail.com>
2025-02-20 22:41:12 +0100

    added reading from files, optimizations still req

--- WordCounter.java
+++ WordCounter.java
@@ -1,13 +1,16 @@
 import java.util.*;
 import java.io.File;
 import java.io.FileReader;
+import java.lang.StringBuilder;
 
 public class WordCounter
 {
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		int st_besed = 1;
+		StringBuilder nizPrebran = new StringBuilder();
+
+		int st_besed = 0;
 
 		System.out.print("Vnesi poljuben niz ki ima vec besed, ali pot do mape: ");
 		String niz = in.nextLine();
@@ -17,7 +20,17 @@ public class WordCounter
 				try{
 					File potDoMape = new File(niz);
 					FileReader bralec = new FileReader(potDoMape);
-					System.out.println("succes!");
+
+					int data = bralec.read();
+
+					System.out.print("Niz v mapi: ");
+					while(data != -1){
+						char znak = (char)data;
+						nizPrebran.append(znak);
+						System.out.print(znak);
+						data = bralec.read();
+					}
+					bralec.close();
 				}
 				catch(Exception e){
 					System.out.println(e.getMessage());
@@ -25,12 +38,14 @@ public class WordCounter
 				break;
 			}
 		}
+		if(){
+
+		}
 		for(int i = 0; i<niz.length(); i++){
 			if(Character.isWhitespace(niz.charAt(i))){
 				st_besed++;
 			}
 		}
-
 		System.out.println("Stevilo besed v nizu: " + st_besed);
 	}
 }
